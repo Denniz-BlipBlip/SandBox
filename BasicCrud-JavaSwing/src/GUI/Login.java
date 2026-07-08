58e996ebf227b35f9b5aae37872d2da925718d94
Denniz <gabrieldenniz@gmail.com>
2026-07-08 22:59:15 +0800

    almost

--- BasicCrud-JavaSwing/src/GUI/Login.java
+++ BasicCrud-JavaSwing/src/GUI/Login.java
@@ -1,6 +1,9 @@
 package GUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.TextField;
 import java.awt.Window;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -13,7 +16,6 @@ public class Login extends JPanel
 {
   public Login()
   {
-    this.field=new Field();
     //this.window=window;
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     setBackground(BG_COLOR);
@@ -22,11 +24,36 @@ public class Login extends JPanel
     JTextField txtField=new JTextField();
     JLabel label=new JLabel("Username");
 
-    add(field.textField(label,txtField,this.FONT_COLOR,this.BG_COLOR));
+    add(this.field(txtField, label));
+  }
+
+  private JPanel field(JTextField field,JLabel label)
+  {
+    JPanel panel=new JPanel();
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    panel.setAlignmentX(CENTER_ALIGNMENT);
+    panel.setBackground(this.BG_COLOR);
+
+    field.setAlignmentX(CENTER_ALIGNMENT);
+    field.setMaximumSize(this.FIELD_SIZE);
+    field.setForeground(this.FONT_COLOR);
+    field.setFont(this.FONT_STYLE);
+
+    label.setAlignmentX(LEFT_ALIGNMENT);
+    label.setFont(this.FONT_TITLE);
+    label.setForeground(this.FONT_COLOR);
+
+    panel.add(label);
+    //panel.add(Box.createVerticalStrut(10));
+    //panel.add(field);
+    //panel.add(Box.createVerticalStrut(15));
+    return panel;
   }
 
   private final Color FONT_COLOR=Color.BLACK;
   private final Color BG_COLOR=Color.RED;
+  private final Dimension FIELD_SIZE=new Dimension(Integer.MAX_VALUE,40);
+  private final Font FONT_STYLE=new Font("Monospaced",100,Font.BOLD);
+  private final Font FONT_TITLE=new Font("Monospaced",100,Font.BOLD);
   private Window window;
-  private Field field;
 }
