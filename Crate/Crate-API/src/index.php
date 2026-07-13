7be589b12a655c41944c5cbc96e03e8631cbc21a
Denniz <gabrieldenniz@gmail.com>
2026-07-13 15:34:33 +0800

    re-config of the Crate project

--- Crate/Crate-API/src/index.php
+++ Crate/Crate-API/src/index.php
@@ -9,11 +9,11 @@ $users = $db->query('SELECT * FROM users')->fetchAll(PDO::FETCH_ASSOC);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Crate</title>
+  <title>crate-api</title>
   <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
-  <h1>Hello from Crate!</h1>
+  <h1>Hello from crate-api!</h1>
   <ul>
     <?php foreach($users as $user): ?>
       <li><?= htmlspecialchars($user['name']) ?></li>
